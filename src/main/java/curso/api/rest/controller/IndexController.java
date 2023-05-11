48547ea7275e1ec8baca81d196bcacee3c89f895
viniciuspch <vinicius.geral2018@gmail.com>
2023-05-11 10:48:05 -0300

    Implementado lógica de criptografia e alteração de senhas criptografadas no endpoint put

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -68,6 +69,8 @@ public class IndexController {
 		for (Telefones telefone : usuario.getTelefones()) {
 			telefone.setUsuario(usuario);
 		}
+		String encryptedPassword = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(encryptedPassword);
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
@@ -76,8 +79,17 @@ public class IndexController {
 	/* Atualiza */
 	@PutMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Usuario> atualizar(@RequestBody(required = true) Usuario usuario) {
+
 		for (Telefones telefone : usuario.getTelefones()) {
 			telefone.setUsuario(usuario);
+		}
+
+		Usuario temporaryUser = usuarioRepository.findUserByLogin(usuario.getLogin());
+
+		if (!temporaryUser.getSenha().equals(usuario.getSenha())) {
+			
+			String encryptedPassword = new BCryptPasswordEncoder().encode(usuario.getSenha());
+			usuario.setSenha(encryptedPassword);
 		}
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 
